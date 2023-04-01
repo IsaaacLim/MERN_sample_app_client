6fa7016f0852d1c3afa82973ca66be793fb60d61
IsaaacLim <isaaclimjj@gmail.com>
2023-04-02 01:03:01 +0800

    fix: home page navigation

--- src/layout/LeftSidebar/LeftSidebarLinks.tsx
+++ src/layout/LeftSidebar/LeftSidebarLinks.tsx
@@ -7,7 +7,7 @@ const LeftSidebarLinks = () => {
     <ul className="mt-6 space-y-2 font-medium">
       <li>
         <a
-          href=""
+          href="/"
           className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
         >
           <FiHome />
