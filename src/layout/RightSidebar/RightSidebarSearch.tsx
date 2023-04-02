b937acec9f4acf718a864e21bdf8db1b02c8d970
IsaaacLim <isaaclimjj@gmail.com>
2023-04-02 23:48:27 +0800

    chore: fix console errors

--- src/layout/RightSidebar/RightSidebarSearch.tsx
+++ src/layout/RightSidebar/RightSidebarSearch.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const LeftSidebarCTA = () => {
+const RightSidebarCTA = () => {
   return (
     <form>
       <label
@@ -20,9 +20,9 @@ const LeftSidebarCTA = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             ></path>
           </svg>
@@ -45,4 +45,4 @@ const LeftSidebarCTA = () => {
   );
 };
 
-export default LeftSidebarCTA;
+export default RightSidebarCTA;
