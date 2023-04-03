0b33642b620202c723d4199b0678aff037b8aadd
IsaaacLim <isaaclimjj@gmail.com>
2023-04-04 01:56:19 +0800

    build: disable react dev tools in production

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import "@/styles/globals.css";
 import "../styles/embla.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools;
 
 const queryClient = new QueryClient();
 
